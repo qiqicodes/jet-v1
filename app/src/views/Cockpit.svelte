1a3a8f19a2910bba9ec58088aa5ef08d33918e93
Eric Su <ericsu@Erics-MBP.local>
2021-09-15 21:05:35 +0200

    prevent users from withdrawing below c-ratio or no deposits

--- app/src/views/Cockpit.svelte
+++ app/src/views/Cockpit.svelte
@@ -102,8 +102,13 @@
         disabledMessage = dictionary[$PREFERRED_LANGUAGE].cockpit.assetIsCurrentBorrow
           .replace('{{ASSET}}', $CURRENT_RESERVE.abbrev);
       }
-    } else if ($TRADE_ACTION === 'withdraw' && !collateralBalances[$CURRENT_RESERVE.abbrev]) {
+    } else if ($TRADE_ACTION === 'withdraw' && (!collateralBalances[$CURRENT_RESERVE.abbrev] || noDeposits || belowMinCRatio )) {
       disabledInput = true;
+      if (noDeposits) {
+        disabledMessage = disabledMessage = dictionary[$PREFERRED_LANGUAGE].cockpit.noDeposits;
+      } else if (belowMinCRatio) {
+        disabledMessage = disabledMessage = dictionary[$PREFERRED_LANGUAGE].cockpit.belowMinCRatio;
+      }
     } else if ($TRADE_ACTION === 'borrow' && (noDeposits || belowMinCRatio || assetsAreCurrentDeposit[$CURRENT_RESERVE.abbrev] || !$CURRENT_RESERVE.availableLiquidity.uiAmountFloat)) {
       disabledInput = true;
       if (noDeposits) {
@@ -177,6 +182,7 @@
              : 1
       );
     }
+    
   };
 
   // Update all market/user data
@@ -224,6 +230,7 @@
     // Set adjusted ratio to current ratio
     if (!adjustedRatio && obligation.colRatio) {
       adjustedRatio = obligation.colRatio;
+      console.log('adjusted c-ratio updated to c-ratio');
     }
 
     // Check if user's current position shouldn't allow trades
@@ -231,6 +238,7 @@
 
     // Get max input value for current trade scenario
     getMaxInputValue();
+    console.log('value updated');
   };
 
   // Check scenario and submit trade
