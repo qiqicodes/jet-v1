d1cb2fe52a93f5bd36126d51325228e4f6f69f1d
rhubarbgarden <32130807+rhubarbgarden@users.noreply.github.com>
2021-09-29 22:33:37 +0000

    Moved Crimea check to helper (#130)
    * Moved Crimea check to helper
    
    * Changed helper function to return boolean only
    
    * Using a normal if statement instead of ternary
    
    Co-authored-by: Tristyn Stimpson <tristynstimpson@gmail.com>

--- app/src/scripts/localization.ts
+++ app/src/scripts/localization.ts
@@ -11,6 +11,14 @@ import * as Jet_Definitions_RU from './languages/Jet_Definitions_RU.json';
 import * as Jet_UI_TR from './languages/Jet_UI_TR.json';
 import * as Jet_Definitions_TR from './languages/Jet_Definitions_TR.json';
 
+const crimeaCheck = (locale: Locale): boolean => {
+  const ifCrimea: string = locale?.postal.toString().substring(0, 2);
+  if (ifCrimea === "95" || ifCrimea === "96" || ifCrimea === "97" || ifCrimea === "98") {
+    return true;
+  } else {
+    return false
+  }
+}
 
 // Get user's preferred language from browser
 // Use fallback if not
@@ -37,8 +45,9 @@ export const getLocale = async (): Promise<void> => {
         // If country is Ukraine, checks if first two digits
         // of the postal code further match Crimean postal codes.
         if (locale?.country === "UA") {
-          const ifCrimea: string = locale?.postal.toString().substring(0, 2);
-          ifCrimea === ("95" || "96" || "97" || "98") ? INIT_FAILED.set({ geobanned: true }) : null
+          if(crimeaCheck(locale)) {
+            INIT_FAILED.set({ geobanned: true })
+          }
         } else {
           INIT_FAILED.set({ geobanned: true });
         }
